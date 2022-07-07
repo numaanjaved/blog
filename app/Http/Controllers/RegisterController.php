96e24d8098a02869cf0e44490d6bb6cf2798ae18
numaanjaved <naumaan.javed@hotmail.com>
2022-07-07 19:59:32 +0500

    registration done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -9,5 +10,15 @@ class RegisterController extends Controller
     //
     public function create(){
         return view('register.create');
+    }
+    public function store(){
+        $attributes = request()->validate([
+            'username' => 'required|max:255',
+            'name' => 'required',
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        User::create($attributes);
+        return redirect('/');
     }
 }
